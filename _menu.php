76c8005a21f09a799270616915d60848f7ad0c40
cokeito <nivallejos87@gmail.com>
2018-08-24 10:15:15 -0300

    cambio de html a php para poder usar el menu

---
+++ _menu.php
@@ -0,0 +1,29 @@
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="servicios.php">Servicios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blog.php">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="noticias.php">Noticias</a>
+              </li>              
+              <li class="nav-item">
+                <a class="nav-link" href="simulador.php">Simulador de Multa</a>
+              </li>                        
+
+              <li class="nav-item">
+                <a class="nav-link" href="contacto.php">Contacto</a>
+              </li>                        
+
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+              <button class="btn btn-outline my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+          </div>
--- contacto.php
+++ contacto.php
@@ -34,32 +34,7 @@
             <span class="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Empresa</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Servicios</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Simulador de Multa</a>
-              </li>                        
-
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contacto</a>
-              </li>                        
-
-
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
-              <button class="btn btn-outline my-2 my-sm-0" type="submit">Buscar</button>
-            </form>
-          </div>
+          <?php include('_menu.php');?>
         </nav>
       </div>
     </div> 
