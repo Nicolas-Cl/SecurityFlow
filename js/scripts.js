1028b56a1b4429279fc488c58ec3b64698060cd7
Nicolas Vallejos <nivallejos87@gmail.com>
2018-08-24 04:28:21 -0300

    agregue plugin jGrowl

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,28 @@
 
 var evaluar_simulador = function() {
 
+  $.bootstrapGrowl("Calculando Infracciones");
   var utm 
   $.getJSON('https://mindicador.cl/api', function(data) {
     var dailyIndicators = data;
 
     var utm = dailyIndicators.utm.valor;
     var veces = $(".form-control option:selected[value=2]").length;
-    var multa = utm * veces;
 
-    console.log(multa);
+    var valorMulta = 0;
+    $.each( $(".form-control option:selected[value=2]"), function(k,v){
+      valorMulta = valorMulta + parseInt($(this).attr('rel'));
+    })
+    console.log(valorMulta);
 
+    var multa = utm * valorMulta;
+    
     $('#nVeces').html(veces);
     $('#vUTM').html('$'+utm);
+    $('#vTotalUTM').html(valorMulta);
     $('#vTotal').html('$'+multa);
+    
+    $('.oculto').slideDown('fast');
   }).fail(function() {
     console.log('Error al consumir la API!');
   });
